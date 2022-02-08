6ddf35bcdbd991af7fcb0b9bd4ba4f4ec2f81cf6
datiko1115 <datiko1115@gmail.com>
2022-02-08 18:12:04 +0900

    Active Storageの導入

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -23,7 +23,6 @@ class RoomsController < ApplicationController
 
   private
 
-  def room_params
-    params.require(:room).permit(:name, user_ids: [])
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
-end
